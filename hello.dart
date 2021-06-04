6b8559cad4539edae2ef873827f2866833e3d54a
paozanwa22 <fausanwadi@gmail.com>
2021-06-05 00:05:00 +0800

    input output

--- hello.dart
+++ hello.dart
@@ -1,12 +1,9 @@
+import 'dart:io';
+
 void main() {
-  const double PI = 3.14;
-  const MAX = 100;
+  
 
-// final = yang dibuat hanya sekali
-  final String BAHASA = 'dart';
-  final VERSI = '2.9.2';
-
-  print('nilai PI adalah :' + PI.toString());
-  print(BAHASA + ' ' + VERSI);
+var a = stdin.readLineSync();
+  print(a);
 
 }
