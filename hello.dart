5d75ab5cc8ffccca11fb3f83216e4129abc4445d
paozanwa22 <fausanwadi@gmail.com>
2021-06-04 22:31:23 +0800

    Variable

---
+++ hello.dart
@@ -0,0 +1,14 @@
+main() {
+  double panjang;
+  double lebar;
+  double luas;
+
+  panjang = 10.0;
+  lebar = 8.0;
+
+  luas = panjang * lebar;
+
+  //toString() //mengkompersi dari double ke string
+  print('luas persegi panjang : ' + luas.toString()); 
+
+}
